5f31112d7c64a1e6dd6437ef9db7558fe1ea02bc
KianIsNotAvailable <kianxw@gmail.com>
2023-11-10 15:15:55 +0000

    making a react weather app

---
+++ src/components/bottom.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function bottom() {
+  return (
+    <div>
+        <div>
+            <img></img>
+            <h3></h3>
+            <p></p>
+        </div>
+        <div>
+            <img></img>
+            <h3></h3>
+            <p></p>
+        </div>
+    </div>
+  )
+}
---
+++ src/components/display.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function display() {
+  return (
+    <div>
+        <img></img>
+        <h1>9°C</h1>
+        <h2>Newcastle</h2>
+    </div>
+  )
+}
