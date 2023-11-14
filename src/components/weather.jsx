8879d846be4ae70cec66a887ded243ba200d4d8f
KianIsNotAvailable <kianxw@gmail.com>
2023-11-14 14:23:13 +0000

    it works, now just style

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -7,13 +7,13 @@ import Search from '../images/search.png'
 export default function Weather() {
     const API_KEY = '7199c791f5656a6336d69d42f8eab69b';
     const API = `https://api.openweathermap.org/data/2.5/weather?q=`;
-    let location ='';
-    let windSpeed = '';
-    let humidity = '';
-    let temperature = '';
     const [inputValue, setInputValue] = useState('');
     const [assignedValue, setAssignedValue] = useState('');
-
+    const [nameValue, setNameValue] = useState('');
+    const [temperatureValue, setTemperatureValue] = useState('');
+    const [windValue, setWindValue] = useState('');
+    const [humidityValue, setHumidityValue] = useState('');
+    const [style, setStyle] = useState(true);
     const handleInputChange = (e) => {
         setInputValue(e.target.value);
     };
@@ -23,7 +23,7 @@ export default function Weather() {
     };
 
     
-
+    
 
 
    async function checkWeather(city){
@@ -31,58 +31,77 @@ export default function Weather() {
     
         
         if(response.status === 404){
-            document.getElementById("display").style.display = "none";
-            document.getElementById("bottom").style.display = "none";
-            document.getElementById('notFound').style.display ='block';
+            setStyle(false)
         } else if (response.status === 400){
-            document.getElementById("display").style.display = "none";
-            document.getElementById("bottom").style.display = "none";
-            document.getElementById('notFound').style.display ='block';
+            setStyle(false)
         } else {
             var data = await response.json();
             console.log(data);
-            document.getElementById("display").style.display = "block";
-            document.getElementById("bottom").style.display = "block";
-            document.getElementById('notFound').style.display ='none';
+            setStyle(true);
+            
     
+            const handleNameValue = () => {
+                setNameValue(data.name)
+            }
+            const handleTemperatureValue = () => {
+                setTemperatureValue(Math.round(data.main.temp - 273.15) + '°C')
+            }
+            const handleWindValue = () => {
+                setWindValue(data.wind.speed + "km/h")
+            }
+            const handleHumidityValue = () => {
+                setHumidityValue(data.main.humidity + "%")
+            }
+            handleNameValue();
+            handleTemperatureValue();
+            handleWindValue();
+            handleHumidityValue();
+
     
-    location = data.name;
-    windSpeed = data.wind.speed + "km/h";
-    humidity = data.main.humidity + "%";
-    temperature = Math.round(data.main.temp) + '°C';
     
         }  
    }
-    const styleBlock = {
-        display: 'none'
-    }
+    
 
   return (
     <div>
+        { style ?
+        <div>
         <div className='search m-auto flex flex-row  space-x-4'>
         <input id='input' type='text' value={inputValue} onChange={handleInputChange}  placeholder='Enter a City' className='text-black rounded-lg backdrop-blur-sm bg-none border-none focus:border-none'></input>
         <button onClick={handleAssignValue}><img  id='button' src={Search} className='h-10 cursor-pointer bg-white rounded-full w-auto p-2'></img></button>
     </div>
     <div id='display' className='flex flex-col justify-center items-center'>
         <img id='icon' src={Clear} className='h-20 w-20'></img>
-        <h1>{temperature}</h1>
-        <h2>{location}</h2>
+        <h1>{temperatureValue}</h1>
+        <h2>{nameValue}</h2>
     </div>
     <div id='bottom' className='flex flex-row items-center justify-center space-x-10 py-20'>
         <div className='flex flex-row space-x-3'>
             <img src={Wind} className='h-10 w-10'></img>
-            <h3>{windSpeed}</h3>
+            <div className='flex flex-col'>
+            <h3>{windValue}</h3>
             <p>Wind Speed</p>
+            </div>
         </div>
         <div className='flex flex-row space-x-3'>
             <img src={Humidity} className='h-10 w-10'></img>
-            <h3>{humidity}</h3>
+            <div className='flex flex-col'>
+            <h3>{humidityValue}</h3>
             <p>Humidity</p>
-        </div>
-        <div id='notFound' style={styleBlock}>
-            <h1>Not a City, Check your spelling</h1>
+            </div>
         </div>
     </div>
+    </div> :
+    <div>
+    <div className='search m-auto flex flex-row  space-x-4'>
+    <input id='input' type='text' value={inputValue} onChange={handleInputChange}  placeholder='Enter a City' className='text-black rounded-lg backdrop-blur-sm bg-none border-none focus:border-none'></input>
+    <button onClick={handleAssignValue}><img  id='button' src={Search} className='h-10 cursor-pointer bg-white rounded-full w-auto p-2'></img></button>
+</div>
+    <div id='notFound' >
+            <h1>Not a City, Check your spelling.</h1>
+        </div>
+        </div>}
     </div>
   )
 }
